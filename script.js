d58dab2440b27031fcbe2d1937baff9aafad4ed2
Lissandrodev <Luismexico2006@gmail.com>
2025-11-01 16:03:42 -0700

    mejoras en la interfaz y funcionalidad, aun no funcionan los botones

--- script.js
+++ script.js
@@ -1,95 +1,81 @@
 let tiempo = 31;
-const regresiva = setInterval(() => {
-  let elementoAlarma = document.getElementById("audioAlarma");
-  let elementotempo = document.getElementById("tempo");
-  tiempo--;
-  elementotempo.textContent = tiempo;
+let regresiva;
+function iniciarTemporizador() {
+  
+  regresiva = setInterval(() => {
+    let elementoAlarma = document.getElementById("audioAlarma");
+    let elementotempo = document.getElementById("tempo");
+    tiempo--;
+    elementotempo.textContent = tiempo;
+    if (tiempo == 5) {
+      elementotempo.style.color = "red";
+    }
+    if (tiempo <= 0) {
+      clearInterval(regresiva); // parar el intervalo
+      elementotempo.textContent = "Se acabo!";
+      elementotempo.style.color = "red";
 
-  if (tiempo==5) {
-    elementotempo.style.color = "red";
-  }
-
-  if (tiempo <= 0) {
-    clearInterval(regresiva); // parar el intervalo
-    elementotempo.textContent = "Se acabo!";
-    elementotempo.style.color = "red";
-    elementoAlarma.play();
-    alert("Game over");
-  }
-}, 1000);
-
-//Mostrar en pantalla las repuestas y la fecha de hoy
-
-function preguntas() {
-  /*
-  Aqui haremoos un array de preguntas cortas para hacer mas variado el programa..
-  */
-  let arrayPreguntas=[];
+      elementoAlarma.play();
+      alert("Game over");
+    }
+  }, 1000);
 }
+// simulando un media querie, mala practica, pero por el momento es lo mas optimo
+const media = window.matchMedia("(max-width: 40rem)");
+function agregarBotones(e) {
+  if (e.matches) {
+    let agregar = document.querySelector('.contenedor');
+    let boton1 = document.createElement('BUTTON');
+    let boton2 = document.createElement('BUTTON');
+    boton1.classList.add('boton');
+    boton1.textContent = 'Reintentar'
+    boton2.classList.add('boton');
+    boton2.textContent = 'Terminar'
+    let contenedor = document.createElement('DIV');
+    contenedor.classList.add('contenedor_mediacel')
+    contenedor.append(boton1,boton2);
+    agregar.append(contenedor);
+  }
+}
+media.addEventListener("change",agregarBotones);
 
 function fincuest() {
-  console.log("me has presionado");
-  let repuesta1 = document.getElementById("pre1").value;
-  let repuesta2 = document.getElementById("pre2").value;
-  let repuesta3 = document.getElementById("pre3").value;
-  let repuesta4 = document.getElementById("pre4").value;
-  let repuesta5 = document.getElementById("pre5").value;
-
   const fecha = new Date(); // Crear el objeto date de donde extraeremos la fecha y hora
+  // comprobar todos los campos
+  let respuestas = [];
+  let impresion = ""
+  document.querySelectorAll("input[name='pre']").forEach((e) => {
+    respuestas.push(e.value); 
+  });
 
-
-
-  //Si las respuestas estan vacias.
-  if (!repuesta1 || !repuesta2 || !repuesta3 || !repuesta4 || !repuesta5) {
-    alert("Por lo menos intentalo");
-    return;
-  } else {
-    alert(
-      " Respuesta 1 " +
-        repuesta1 +
-        "\n" +
-        " Respuesta 2 " +
-        repuesta2 +
-        "\n" +
-        " Respuesta 3 " +
-        repuesta3 +
-        "\n" +
-        " Respuesta 4 " +
-        repuesta4 +
-        "\n" +
-        " Respuesta 5 " +
-        repuesta5 +
-        "\n" +
-        " Fecha de hoy " +
-        dia_hoy
-    );
-  console.log("funcion")
-
-  }
-
-  //Pausar el tiempo si el boton es presionado (si las repuestas fueron introducidas)
-  if (repuesta1 || repuesta2 || repuesta3 || repuesta4 || repuesta5 != null) {
-    alert("Cuenta regresiva pausada.");
-    clearInterval(regresiva); // detener conteo cuando se contesten todas
+  let vacio = 0
+  respuestas.forEach(e=>{
+    if (e == '') {
+      vacio ++
+    }
+  })
+  const opcionesinValidas = [1,2,3,4,5,6];
+  if (opcionesinValidas.includes(vacio)) {
+    alert('Debes ingresar todas las respuestas');
     return;
   }
+  else {
+    respuestas.forEach((e) => {
+      if (e) {
+        impresion += `Respuesta ${e}\n`;
+      }
+    });
+  }
+  alert("tiempo pausado");
+  clearInterval(regresiva)
+  alert(`Aqui las repuestas\nFecha\n${fecha.toDateString("es-mx")}\n${impresion}`);
 }
-("\n");
-//reiniciar el navegador cuando se precione el boton intentar de nuevo
-
-
-
 function reiniciar() {
   alert("Juego Reiniciado");
   location.reload(); // se logra con esto
 }
-// reiniciar funcion
-
-//Usar INNER?
-
-//`${edad_dias} <span>Días</span>`
-
-/*
-let fecha = new Date();
-Por afuera se investigo el toLocateDateString(es-mx);
-*/
+let boton = document.querySelector('#startemp');
+boton.addEventListener('click',( )=>{
+iniciarTemporizador();
+agregarBotones(media);
+})
